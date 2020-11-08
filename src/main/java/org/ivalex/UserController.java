1072b1c4a970be62e2deec72cd56fe93a0074ff8
iv_al3x <tripanosomas@ya.ru>
2020-11-08 08:46:37 +0300

    0.0.3

--- src/main/java/org/ivalex/UserController.java
+++ src/main/java/org/ivalex/UserController.java
@@ -1,12 +1,15 @@
 package org.ivalex;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/people")
 public class UserController {
 
     private UserService service;
@@ -15,7 +18,7 @@ public class UserController {
         this.service = service;
     }
 
-    @GetMapping("/hi")
+    @GetMapping("")
     public ModelAndView hello() {
 
         ModelAndView mav = new ModelAndView("index");
@@ -24,5 +27,27 @@ public class UserController {
 
         mav.addObject("message", userList);
         return mav;
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("person", service.showById(id));
+        return "/show";
+    }
+
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+
+        model.addAttribute("person", new User());
+        return "new_user";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") User user) {
+
+        service.add(user);
+        return "redirect:/people";
     }
 }
--- src/main/java/org/ivalex/UserService.java
+++ src/main/java/org/ivalex/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,5 +14,15 @@ public class UserService {
 
     public List<User> listAll() {
         return (List<User>) repo.findAll();
+    }
+
+    public void add(User user) {
+        repo.save(user);
+    }
+
+    public User showById(Long id) {
+        Optional<User> optionalUser = repo.findById(id);
+        User user = optionalUser.orElse(null);
+        return user;
     }
 }
