87f9f1e62c4a9231c3beb0e12067b4b2c346eef5
iv_al3x <tripanosomas@ya.ru>
2020-11-09 13:07:35 +0300

    0.0.5. Still works

--- src/main/java/org/ivalex/config/JpaConfig.java
+++ src/main/java/org/ivalex/config/JpaConfig.java
@@ -1,21 +1,48 @@
 package org.ivalex.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScan(value = "org.ivalex")
 @EnableJpaRepositories(basePackages = {"org.ivalex"})
 public class JpaConfig {
+
+    @Autowired
+    private Environment env;
+
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
 
     @Bean
     public LocalEntityManagerFactoryBean entityManagerFactory() {
 
         LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+
         factoryBean.setPersistenceUnitName("UsersDB");
 
         return factoryBean;
@@ -28,4 +55,6 @@ public class JpaConfig {
         transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
     }
+
+
 }
